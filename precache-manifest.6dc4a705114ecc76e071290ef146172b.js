d29591d8ff3def4592c3b45bac19dbbfb2365766
JMutch <jmutch@cryptocompare.com>
2020-06-17 14:50:44 +0100

    Updates

---
+++ precache-manifest.6dc4a705114ecc76e071290ef146172b.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7e6ac57e600ef0e476227514a8c3f91",
+    "url": "/orderbook-streamer/index.html"
+  },
+  {
+    "revision": "98a9d3e14a38887bf9ad",
+    "url": "/orderbook-streamer/static/css/3.de424728.chunk.css"
+  },
+  {
+    "revision": "203153002c8a5a5bc5f0",
+    "url": "/orderbook-streamer/static/css/main.1ccb61db.chunk.css"
+  },
+  {
+    "revision": "98a9d3e14a38887bf9ad",
+    "url": "/orderbook-streamer/static/js/3.4d976a1f.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/orderbook-streamer/static/js/3.4d976a1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0496316960a8fc017a4b",
+    "url": "/orderbook-streamer/static/js/4.9c807c91.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/orderbook-streamer/static/js/4.9c807c91.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e356180df4e362660568",
+    "url": "/orderbook-streamer/static/js/5.4191ac33.chunk.js"
+  },
+  {
+    "revision": "c2703eee005e82dd04f5f76ec1204181",
+    "url": "/orderbook-streamer/static/js/5.4191ac33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "748d4aca56aa90dc9483",
+    "url": "/orderbook-streamer/static/js/6.b58bc119.chunk.js"
+  },
+  {
+    "revision": "203153002c8a5a5bc5f0",
+    "url": "/orderbook-streamer/static/js/main.2d6edb1d.chunk.js"
+  },
+  {
+    "revision": "49665dd290a2c806345e",
+    "url": "/orderbook-streamer/static/js/runtime-main.c7df446f.js"
+  },
+  {
+    "revision": "c469fbd050cb2096448f",
+    "url": "/orderbook-streamer/static/js/xlsx.b970c454.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/orderbook-streamer/static/media/logo.5d5d9eef.svg"
+  }
+]);
